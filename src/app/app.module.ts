35a6b7c64214accbeee422ecab6236f5f07b7159
yeancarlos <yeancarlos44@gmail.com>
2018-02-07 11:46:02 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { CategoryComponent } from './category/category.component';
+import { DistrictComponent } from './district/district.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductComponent,
+    MarketingComponent,
+    CategoryComponent,
+    DistrictComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/marketing/marketing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketing',
+  templateUrl: './marketing.component.html',
+  styleUrls: ['./marketing.component.sass']
+})
+export class MarketingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
