fcac4ed01ae63aec4a64a1b4872412926d6ef74b
yeancarlos <yeancarlos44@gmail.com>
2018-02-08 18:30:13 -0500

    update

--- src/app/marketing/marketing.component.ts
+++ src/app/marketing/marketing.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-marketing',
   templateUrl: './marketing.component.html',
-  styleUrls: ['./marketing.component.sass']
+  styleUrls: ['./marketing.component.sass'],
+  providers : [AppService]
 })
 export class MarketingComponent implements OnInit {
 
-  constructor() { }
+  public listaDireccion: any[] = [];
+  public listaProducto: any[] = [];
+  constructor(private Services: AppService) { }
 
   ngOnInit() {
+    this.Services.getDistrito().subscribe(rest => {
+      //console.log(rest.json());
+      this.listaDireccion = rest.json();
+      console.log(this.listaDireccion);
+    });
   }
 
 }
